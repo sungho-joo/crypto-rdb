63b663360d970f09007c606ea504c8e9ed899850
dongminlee94 <kid33629@gmail.com>
2022-03-04 22:42:16 +0900

    Complete final code of select_data.py

--- src/db/select_data.py
+++ src/db/select_data.py
@@ -8,57 +8,52 @@ Author:
     Email: kid33629@gmail.com
 """
 
-from typing import Any, List
+from typing import Any, Dict, List, Tuple
 
 import sqlalchemy as db
 from sqlalchemy.orm import Session
 
 from src.db.create_tables import Accum, Ticker, Trade, engine
 
-# start_date = " "
-# start_time = " "
-# end_date = " "
-# end_time = " "
+START_DATE = "2022-03-04"
+START_TIME = "12:33:07"
+END_DATE = "2022-03-04"
+END_TIME = "12:37:39"
 
 
-def select_from_tables(stmt: db.sql.Select) -> List[Any]:
+def select_from_tables(stmt: db.sql.Select) -> Dict[int, Tuple[Any, ...]]:
     """Select data from tables"""
+    record_dict = {}
     with Session(engine) as session:
         print(f"Columns: {list(session.execute(stmt).keys())}")
-        records = session.execute(stmt).scalars().first()
-        print(records)
-        # for i, record in enumerate(records):
-        #     print(f"{i}: {record}")
-    return records
+        for i, record in enumerate(session.execute(stmt)):
+            print(f"{i}: {record}")
+            record_dict[i] = tuple(record)
+    return record_dict
 
 
 if __name__ == "__main__":
     stmt = db.select(Ticker.id)
-    ticker_ids = select_from_tables(stmt)
+    ticker_ids: List[int] = [int(ticker_id) for ticker_id in select_from_tables(stmt)]
 
     trade_prices = {}
     for ticker_id in ticker_ids:
         stmt = db.select(Trade.trade_price).where(Trade.ticker_id == ticker_id)
-        trade_price = select_from_tables(stmt)
-        trade_prices[ticker_id] = trade_price
+        trade_prices[ticker_id] = select_from_tables(stmt)
 
     acc_bid_volumes = {}
     for ticker_id in ticker_ids:
         stmt = (
             db.select(Trade.trade_date, Trade.trade_time, Accum.acc_bid_volume)
             .join(Accum, Trade.id == Accum.id)
-            .where(Trade.ticker_id == ticker_id)
+            .where(
+                db.and_(
+                    Trade.ticker_id == ticker_id,
+                    START_DATE <= Trade.trade_date,
+                    START_TIME <= Trade.trade_time,
+                    Trade.trade_date <= END_DATE,
+                    Trade.trade_time <= END_TIME,
+                )
+            )
         )
-        acc_bid_volume = select_from_tables(stmt)
-        acc_bid_volumes[ticker_id] = acc_bid_volume
-
-
-# 2. 누적 거래량 (기간 설정 가능)
-# select acc_bid_volume from accum where ticker_id = 1;
-# select acc_bid_volume from accum where ticker_id = 2;
-# select acc_bid_volume from accum where ticker_id = 3;
-
-# select trade.trade_date, trade.trade_time, accum.acc_bid_volume
-# from trade, accum where trade.id = accum.id and trade.ticker_id = 1 limit 5;
-
-# 3. MA 10, 20, 50 - graph
+        acc_bid_volumes[ticker_id] = select_from_tables(stmt)
