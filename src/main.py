aa33674e7dea423efb6c445afd427e9ba90ae936
sungho <sungho.joo@makinarocks.ai>
2022-05-11 02:20:45 +0900

    Rebased repository update, validated apis

--- src/main.py
+++ src/main.py
@@ -1,30 +1,16 @@
 """Main function initializing fastapi app"""
 
-import sys
-from typing import Final
-
 from fastapi import FastAPI
-
-from db.database import Database
 from src.stat import controller as stat_controller
 
 
 def create_app() -> FastAPI:
     """Create and do initial configuration of fastapi app"""
 
-    #    db = Database()
-    # try:
-    #     db.create_database()
-    # except Exception:  # pylint: disable=broad-except
-    #     sys.exit(1)
-
     app_ = FastAPI()
 
     # Add routers
     app_.include_router(stat_controller.router)
-    # Overide exception handlers
-
-    # Override default class method
 
     return app_
 
--- src/stat/controller.py
+++ src/stat/controller.py
@@ -22,14 +22,19 @@ router = APIRouter(prefix="/stat", tags=["stat"], responses={404: {"description"
 @router.get("/tickers")
 def get_tickers():
     """Get all tickers"""
-    tickers = repository.get_all_tickers()
+    tickers = repository.get_all_market_codes()
     return tickers
 
 
 @router.get("/")
-def get_all_stat_data_for_given_tickers():
+def get_all_stat_data_for_given_tickers(
+    query_params: Optional[List[str]] = Query(None, description="List of market code")
+):
     """Get all data for given tickers"""
-    ticker_ids = repository.get_all_ticker_ids()
+    if not query_params:
+        ticker_ids = repository.get_all_market_ids()
+    else:
+        ticker_ids = repository.get_some_market_ids(query_params)
     ans = []
     for ticker_id in ticker_ids:
         all_stat_data: Tuple[Dict[int, Tuple[Any, ...]], List[str]] = repository.get_all_data(ticker_id)
@@ -38,24 +43,30 @@ def get_all_stat_data_for_given_tickers():
 
 
 @router.get("/price/")
-def get_current_price(query_param: Optional[List[str]] = None):
-    if not query_param:
-        ticker_ids = repository.get_all_ticker_ids()
+def get_current_price(
+    query_params: Optional[List[str]] = Query(None, description="List of market code")
+):
+    if not query_params:
+        ticker_ids = repository.get_all_market_ids()
     else:
-        ticker_ids = repository.get_ticker_ids(query_param)
+        ticker_ids = repository.get_some_market_ids(query_params)
     ans = []
-    for ticker_id in ticker_idx:
+    for ticker_id in ticker_ids:
         price, _ = repository.get_trade_prices(ticker_id)
         ans.append(price)
     return ans
 
 
 @router.get("/ask/accumulation/{start_date}-{end_date}")
-def get_ask_accumlation_range(start_date: str, end_date: str, query_param: Optional[List[str]] = None):
-    if not query_param:
-        ticker_ids = repository.get_all_ticker_ids()
+def get_ask_accumlation_range(
+    start_date: str,
+    end_date: str,
+    query_params: Optional[List[str]] = Query(None, description="List of market code"),
+):
+    if not query_params:
+        ticker_ids = repository.get_all_market_ids()
     else:
-        ticker_ids = repository.get_ticker_ids(query_param)
+        ticker_ids = repository.get_some_market_ids(query_params)
 
     ans = []
     for ticker_id in ticker_ids:
@@ -65,11 +76,15 @@ def get_ask_accumlation_range(start_date: str, end_date: str, query_param: Optio
 
 
 @router.get("/bid/accumulation/{start_date}-{end_date}")
-def get_bid_accumlation_range(start_date: str, end_date: str, query_param: Optional[List[str]] = None):
-    if not query_param:
-        ticker_ids = repository.get_all_ticker_ids()
+def get_bid_accumlation_range(
+    start_date: str,
+    end_date: str,
+    query_params: Optional[List[str]] = Query(None, description="List of market code"),
+):
+    if not query_params:
+        ticker_ids = repository.get_all_market_ids()
     else:
-        ticker_ids = repository.get_ticker_ids(query_param)
+        ticker_ids = repository.get_some_market_ids(query_params)
 
     ans = []
     for ticker_id in ticker_ids:
