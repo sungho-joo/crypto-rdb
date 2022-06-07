5d3d1144eab62a42d7375c09d7065f4210ccc39d
sungho <sungho.joo@makinarocks.ai>
2022-06-07 23:47:38 +0900

    SDK initial version

--- src/crypto/sdk/data_getter.py
+++ src/crypto/sdk/data_getter.py
@@ -24,14 +24,38 @@ class DataGetter:
     ) -> None:
         self._url = url
         self.market_codes = market_codes
+        self._stats = ["price", "bid_accumulation", "ask_accumulation"]
 
-    def get_price_data(self, start_date: str, end_date: str) -> str:
-        """Get data"""
+    @property
+    def stats(self) -> List[str]:
+        """Get stats"""
+        return self._stats
+
+    @classmethod
+    def _get_stat_name(cls, stat_name: str) -> str:
+        stat_dict = {
+            "price": "price",
+            "bid_accumulation": "bid/accumulation",
+            "ask_accumulation": "ask/accumulation",
+        }
+        return stat_dict[stat_name]
+
+    def get_stat_data(self, stat_name: str, start_date: str, end_date: str) -> str:
+        """Get stat"""
         headers = {"Content-Type": "application/json"}
-        url = self._url + "/stat/price/"
+        url = self._url + "/stat/" + self._get_stat_name(stat_name) + "/"
         payload = self._get_payload(start_date, end_date)
+
         response = requests.post(url, headers=headers, json=payload)
-        return response.json()
+        status_code, response_body = (
+            response.status_code,
+            response.json(),
+        )
+
+        if status_code == 200:
+            return response_body
+        else:
+            return f"Error occured while getting data: {response_body}"
 
     def _get_payload(self, start_date: str, end_date: str) -> Dict[str, Any]:
         return {
